50eb674599a97504ec029efd6e363e4903587f28
garfieldel <closetlionel@gmail.com>
2026-08-29 22:38:05 -0500

    Update get_msbuild_path.bat

--- framework/external/get_msbuild_path.bat
+++ framework/external/get_msbuild_path.bat
@@ -1,8 +1,8 @@
 @echo off
 
 :: You might need to change this path to point to your Visual Studio installation's MSBuild.exe
-set msbuildPathCom="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-set msbuildPathPro="C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe"
+set msbuildPathCom=msbuild
+set msbuildPathPro=msbuild
 
 if exist %msbuildPathPro% (
 	set msbuildPath=%msbuildPathPro%
